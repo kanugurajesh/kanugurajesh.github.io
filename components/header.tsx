60278ff9e3c54f42bf55c46a1f680c03d17ccbea
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-04 22:07:05 +0530

    Adding the contants so that people can build their own clone of it

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import { Blog_Name } from '../lib/constants'
 
 const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
-        DevShare
+        {Blog_Name}
       </Link>
       .
     </h2>
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,10 +1,10 @@
-import { CMS_NAME } from '../lib/constants'
+import { CMS_NAME, Blog_Name } from '../lib/constants'
 
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        DevShare
+        {Blog_Name}
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A blog where <a href="https://rajeshportfolio.me" className='font-bold underline underline-offset-6' target='_blank'>I</a> share my thoughts on <b>web</b>, <b>mobile</b> and <b>python</b> development.
