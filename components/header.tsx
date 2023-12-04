ae5e85bbff5a92351e2fe5e68f7193a1cdf413fc
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-04 21:37:18 +0530

    Changing some typos in the project

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
-        Rajesh-Blog
+        DevShare
       </Link>
       .
     </h2>
