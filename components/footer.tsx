b5e34514e46d0417a781d7a36e85f13714b43158
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-09 15:43:31 +0530

    Adding the blogs

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,8 @@ import Container from './container'
 import { EXAMPLE_PATH } from '../lib/constants'
 import { LinkedIn_URL } from '../lib/constants'
 import { GitHub_URL } from '../lib/constants'
+import styles from '../styles/footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
@@ -11,18 +13,22 @@ const Footer = () => {
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Crafted by Rajesh
           </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2 gap-5">
             <a
               href={LinkedIn_URL}
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              View Linkedin
+              <Image src="/assets/blog/icons/linkedin.png" alt="linkedin" width={50} height={50} />
             </a>
             <a
               href={GitHub_URL}
-              className="mx-3 font-bold hover:underline"
             >
-              View Github
+              <Image src="/assets/blog/icons/github.png" alt="github" width={50} height={50} />
+            </a>
+            <a
+              href={LinkedIn_URL}
+              className="bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors rounded-sm"
+            >
+              Contact Me
             </a>
           </div>
         </div>
