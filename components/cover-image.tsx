714b0e18daccb51728efd685137104cdf8f269c4
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-09 13:03:27 +0530

    Adding some blogs

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -14,10 +14,10 @@ const CoverImage = ({ title, src, slug }: Props) => {
       src={src}
       alt={`Cover Image for ${title}`}
       className={cn('shadow-sm w-full', {
-        'hover:shadow-lg transition-shadow duration-200': slug,
+        'hover:shadow-lg transition-shadow duration-200 rounded-md': slug,
       })}
       width={500}
-      height={400}
+      height={500}
     />
   )
   return (
