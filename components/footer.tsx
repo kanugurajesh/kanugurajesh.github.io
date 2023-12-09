8b61b97cf5649abfdbeec743370a5f1bc206cd1d
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-09 15:47:07 +0530

    Adding logos

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,11 +9,11 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="py-28 flex flex-col lg:flex-row items-center justify-between">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Crafted by Rajesh
           </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2 gap-5">
+          <div className="flex flex-col lg:flex-row justify-center items-center gap-5">
             <a
               href={LinkedIn_URL}
             >
