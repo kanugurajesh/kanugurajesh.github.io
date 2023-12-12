e53f040bed65f3379dfaa6af917be782867d1d51
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-12 12:36:23 +0530

    Changing the description

--- components/meta.tsx
+++ components/meta.tsx
@@ -34,7 +34,7 @@ const Meta = () => {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={`Hi I am Rajesh Explore my blog on web, azure, python, web3.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
