6f8160dcb463f2235f4373af084ff758d05ead8b
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-13 19:43:36 +0530

    Changing the styling of the contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 
 export default function Contact() {
     return (
-        <div className="w-screen h-screen text-extrabold flex justify-around font-black">
-            <div className="flex flex-col justify-around">
+        <div className="w-screen h-screen text-extrabold flex justify-around items-center">
+            <div className="flex flex-col justify-around gap-40 h-3/4">
                 <div className="flex flex-col gap-5">
                     <h1 className="text-5xl font-black">Get a quote</h1>
                     <p className="text-lg">Fill up the form and I will get back to you within 24 hours</p>
@@ -37,19 +37,19 @@ export default function Contact() {
                     </Link>
                 </div>
             </div>
-            <div className="flex flex-col justify-center gap-10">
+            <div className="flex flex-col justify-center gap-10 border h-3/4 p-10 rounded-2xl border-2">
                 <div className="flex flex-col gap-2">
-                    <label htmlFor="text-input">Name:</label>
-                    <input type="text" id="text-input" className="border border-black border-2 h-10 rounded-md p-2 font-normal" placeholder="Enter the name"/>
+                    <label htmlFor="text-input" className="font-bold">Name:</label>
+                    <input type="text" id="text-input" className="border border-black border-2 h-10 rounded-md p-2 font-normal w-80" placeholder="Enter the name"/>
                 </div>
                 <div className="flex flex-col gap-2">
-                    <label htmlFor="email-input">Email:</label>
-                    <input type="email" id="email-input" className="border border-black border-2 h-10 rounded-md p-2 font-normal" placeholder="Enter the email"/>
+                    <label htmlFor="email-input" className="font-bold">Email:</label>
+                    <input type="email" id="email-input" className="border border-black border-2 h-10 rounded-md p-2 font-normal w-80" placeholder="Enter the email"/>
                 </div>
                 <div className="flex flex-col gap-2">
-                    <label htmlFor="textarea-input">Message:</label>
+                    <label htmlFor="textarea-input" className="font-bold">Message:</label>
                     {/* @ts-ignore */}
-                    <textarea name="" placeholder="Enter the message" id="textarea-input" cols="30" rows="10" className="border border-black border-2 h-40 rounded-md p-2 font-normal">
+                    <textarea name="" placeholder="Enter the message" id="textarea-input" cols="30" rows="10" className="border border-black border-2 h-40 rounded-md p-2 font-normal w-80">
                     </textarea>
                 </div>
             </div>
