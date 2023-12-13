cb9a954836ae3e6295a2859a896c9ae3d2d95766
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-13 23:11:09 +0530

    Adding syntax highlighting

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,61 @@
 import Image from "next/image"
 import Link from "next/link"
+import toast, { Toaster } from "react-hot-toast"
+import { useState } from "react"
 
 export default function Contact() {
+
+    const [name, setName] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+    const [message, setMessage] = useState<string>("")
+
+    // create a function to validate the email
+    const validateEmail = (email: string) => {
+        const re = /\S+@\S+\.\S+/
+        return re.test(email)
+    }
+
+    // create a function to validate the name
+    const validateName = (name: string) => {
+        const re = /^[a-zA-Z ]{2,30}$/
+        return re.test(name)
+    }
+
+    // create a function to validate the message
+    const validateMessage = (message: string) => {
+        const re = /^[a-zA-Z ]{2,30}$/
+        return re.test(message)
+    }
+
+    // create a function to validate the form
+
+    const validateForm = () => {
+        if (!validateName(name)) {
+            toast.error("Please enter the valid name")
+            return false
+        }
+        if (!validateEmail(email)) {
+            toast.error("Please enter the valid email")
+            return false
+        }
+        if (!validateMessage(message)) {
+            toast.error("Please enter the valid message")
+            return false
+        }
+        return true
+    }
+
+    // create a function to handle the submit
+
+    const handleSubmit = () => {
+        if (validateForm()) {
+            toast.success("Your message has been sent successfully")
+        }
+    }
+
     return (
         <div className="w-screen h-screen text-extrabold flex justify-around items-center">
+            <Toaster />
             <div className="flex flex-col justify-around gap-40 h-3/4">
                 <div className="flex flex-col gap-5">
                     <h1 className="text-5xl font-black">Contact us</h1>
@@ -46,20 +98,27 @@ export default function Contact() {
                 </div>
                 <div className="flex flex-col gap-2">
                     <label htmlFor="text-input" className="font-bold">Name:</label>
-                    <input type="text" id="text-input" className="border border-black border-2 h-10 rounded-md p-2 font-normal w-80" placeholder="Enter the name"/>
+                    <input type="text" id="text-input" className="border border-black border-2 h-10 rounded-md p-2 font-normal w-80 focus:ring focus:ring-blue-500/50 focus:ring-offset-2" placeholder="Enter the name" value={name} onChange={(e) => {
+                        setName(e.target.value)
+                    }}/>
                 </div>
                 <div className="flex flex-col gap-2">
                     <label htmlFor="email-input" className="font-bold">Email:</label>
-                    <input type="email" id="email-input" className="border border-black border-2 h-10 rounded-md p-2 font-normal w-80" placeholder="Enter the email"/>
+                    <input type="email" id="email-input" className="border border-black border-2 h-10 rounded-md p-2 font-normal w-80 focus:ring focus:ring-blue-500/50 focus:ring-offset-2" placeholder="Enter the email" value={email} onChange={(e) => {
+                        setEmail(e.target.value)
+                    }}/>
                 </div>
                 <div className="flex flex-col gap-2">
                     <label htmlFor="textarea-input" className="font-bold">Message:</label>
                     {/* @ts-ignore */}
-                    <textarea name="" placeholder="Enter the message" id="textarea-input" cols="30" rows="10" className="border border-black border-2 h-40 rounded-md p-2 font-normal w-80">
+                    <textarea name="" placeholder="Enter the message" id="textarea-input" cols="30" rows="10" className="border border-black border-2 h-40 rounded-md p-2 font-normal w-80 focus:ring focus:ring-blue-500/50 focus:ring-offset-2" value={message} onChange={(e) => {
+                        setMessage(e.target.value)
+                    }}>
                     </textarea>
                 </div>
                 <div>
-                    <button className="w-full font-bold bg-black hover:bg-white hover:text-black text-white px-5 py-3 rounded-md border border-2 transition ease-in-out space-x-4">Send Message</button>
+                    {/* @ts-ignore */}
+                    <button className="w-full font-bold bg-black hover:bg-white hover:text-black text-white px-5 py-3 rounded-md border border-2 transition ease-in-out" onClick={()=>handleSubmit()}>Send Message</button>
                 </div>
             </div>
         </div>
